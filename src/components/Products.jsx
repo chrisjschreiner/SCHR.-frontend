a210c45cf62978d8a15bc75044b67a47ed7d8297
Chris Schreiner <chrisjschreiner@gmail.com>
2023-08-16 14:22:48 -0700

    change fetch from local to onrender

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -18,8 +18,8 @@ const Products = ({ cat, filters, sort }) => {
     const getProducts = async () => {
       try {
         const res = await axios.get(
-          cat && `http://localhost:5000/api/products?category=${cat}`
-          // cat && `https://schr-api.onrender.com/api/products?category=${cat}`
+          // cat && `http://localhost:5000/api/products?category=${cat}`
+          cat && `https://schr-api.onrender.com/api/products?category=${cat}`
         );
         setProducts(res.data);
       } catch (err) {}
